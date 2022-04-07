9528fc2d54aa4d30f739da125fc3338613acade1
Thyrsus12 <cocepsilon@gmail.com>
2022-04-08 00:18:59 +0200

    Map is correctly rendered, but it isn't in the correct place

---
+++ assets/Test Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Test Tileset" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="Sprites para prototipo.png" width="128" height="128"/>
+</tileset>
--- core/src/main/java/com/rotirmar/athena/Main.java
+++ core/src/main/java/com/rotirmar/athena/Main.java
@@ -2,33 +2,60 @@ package com.rotirmar.athena;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
 	private SpriteBatch batch;
-	private Texture image;
+
+	private static final float WORLD_WIDTH = 20;
+	private static final float WORLD_HEIGHT = 20;
+	private static final float BLOCK_SIZE = 1;
+
+	private OrthographicCamera cam;
+	private Viewport viewport;
+
+	private TiledMap tiledMap;
+	private float unitScale;
+	private IsometricTiledMapRenderer mapRenderer;
+
 
 	@Override
 	public void create() {
-		batch = new SpriteBatch();
-		image = new Texture("libgdx.png");
+
+		cam = new OrthographicCamera();
+
+		viewport = new ExtendViewport(10, 10, cam);
+		viewport.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), true);
+
+		tiledMap = new TmxMapLoader().load("testmap.tmx");
+		unitScale = BLOCK_SIZE / 32f;
+		mapRenderer = new IsometricTiledMapRenderer(tiledMap, unitScale);
 	}
 
 	@Override
 	public void render() {
 		Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(image, 140, 210);
-		batch.end();
+
+		mapRenderer.setView(cam);
+		mapRenderer.render();
+
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
-		image.dispose();
+		/*batch.dispose();
+		image.dispose();*/
 	}
 }
