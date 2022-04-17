479d9109f9be4e85cfedc76537aa8446ad4cd4b5
Thyrsus12 <cocepsilon@gmail.com>
2022-04-18 00:07:44 +0200

    render a texture on top of a "selected" tile through of its cell -_('_')_-

--- core/src/main/java/com/rotirmar/athena/Screen.java
+++ core/src/main/java/com/rotirmar/athena/Screen.java
@@ -5,13 +5,13 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
-
-import java.util.ArrayList;
 
 public class Screen extends ScreenAdapter {
 
@@ -26,6 +26,10 @@ public class Screen extends ScreenAdapter {
     private TiledMap tiledMap;
     private float unitScale;
     private IsometricTiledMapRenderer mapRenderer;
+
+    private Texture texture;
+    TiledMapTileLayer.Cell cell;
+    TiledMapTile tile;
 
     public Screen(SpriteBatch batch) {
         this.batch = batch;
@@ -43,6 +47,17 @@ public class Screen extends ScreenAdapter {
         //what a apaño is this
         camera.position.set(WORLD_WIDTH / 2 - 2, WORLD_HEIGHT / 2 - 8, 0);
         camera.update();
+
+        TiledMapTileLayer layer0 = (TiledMapTileLayer) tiledMap.getLayers().get(0);
+        //System.out.println("Layer Width-Height: " + layer0.getWidth() + "-" + layer0.getHeight());
+        //System.out.println("Tile Width-Height: " + layer0.getTileWidth() + "-" + layer0.getTileHeight());
+
+        cell = layer0.getCell(1, 1);
+        tile = cell.getTile();
+        //System.out.println("Tile ID: " + tile.getId());
+        //System.out.println("Pixeles tile: " + tile.getOffsetY());
+
+        texture = new Texture("alex.png");
     }
 
     @Override
@@ -50,10 +65,6 @@ public class Screen extends ScreenAdapter {
         Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        /*batch.setProjectionMatrix(camera.combined);
-        camera.update();
-        batch.begin();
-        batch.end();*/
         handleInput();
 
         int[] layer1 = {0};
@@ -62,6 +73,11 @@ public class Screen extends ScreenAdapter {
         mapRenderer.setView(camera);
         mapRenderer.render(layer1);
         mapRenderer.render(layer2);
+
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(texture, tile.getOffsetX() + 0.32f, tile.getOffsetY() + 0.64f, 0.5f, 0.5f);
+        batch.end();
     }
 
     @Override
