9cf3c85d07da84679fdc4e9e211d97bce002a8a9
Thyrsus12 <cocepsilon@gmail.com>
2022-05-29 19:00:33 +0200

    merged master

--- core/src/main/java/com/rotirmar/athena/MenuScreen.java
+++ core/src/main/java/com/rotirmar/athena/MenuScreen.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import jFrame.FrameworkMenu;
